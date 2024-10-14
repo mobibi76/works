70f86ae37472fe20d8d9070f2e24dca316bd96dd
mobibi76 <snowfall.hamn@gmail.com>
2024-10-15 03:06:20 +0900

    try

--- source/main.js
+++ source/main.js
@@ -199,7 +199,7 @@
             resizeCanvas();
             window.addEventListener('resize', resizeCanvas);
             clearCanvasObjects();
-            for (let i = 0; i < 20; i++) {
+            for (let i = 0; i < 30; i++) {
                 objAction.push({
                     x: Math.random() * canvas.width / window.devicePixelRatio,
                     y: Math.random() * canvas.height / window.devicePixelRatio,
