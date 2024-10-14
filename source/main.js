1647a8c5b05fc43d5742afc239e5c821e15c3b43
mobibi76 <snowfall.hamn@gmail.com>
2024-10-15 02:17:12 +0900

    try

--- source/main.js
+++ source/main.js
@@ -263,7 +263,7 @@
             iframe.srcdoc = `
                 <div style="text-align: center;">
                     <p>Server Response Failure. Try Later.<br>The server may be off.</p>
-                    <img src="../notice/demoLoadFailure.png" alt="Demo Load Failure" style="max-width: 100%; height: auto;">
+                    <img src="../notice/demoLoadFailure.png" alt="Demo Load Failure" style="width: 33%;">
                 </div>
             `;
         }, timeout);
@@ -275,7 +275,7 @@
             iframe.srcdoc = `
                 <div style="text-align: center;">
                     <p>Server Response Failure. Try Later.<br>The server may be off.</p>
-                    <img src="../notice/demoLoadFailure.png" alt="Demo Load Failure" style="max-width: 100%; height: auto;">
+                    <img src="../notice/demoLoadFailure.png" alt="Demo Load Failure" style="width: 33%;">
                 </div>
             `;
         };
