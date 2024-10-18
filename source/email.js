a84c0470669a56350aa5c9b8d2fb89dc55856b93
mobibi76 <snowfall.hamn@gmail.com>
2024-10-18 21:23:52 +0900

    try

---
+++ source/email.js
@@ -0,0 +1,24 @@
+function insertEmail() {
+    const emailElement = document.getElementById('email');
+    if (emailElement) {
+        emailElement.innerHTML = 
+            '<a href="mailto:processdesignbase@gmail.com">processdesignbase@gmail.com</a> (125-51-00257)';
+    } else {
+        console.error('Email Element Not Found.');
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    const footer = document.querySelector('footer');
+    if (footer) {
+        insertEmail();
+    } else {
+        const observer = new MutationObserver(() => {
+            const emailElement = document.getElementById('email');
+            if (emailElement) {
+                insertEmail();
+                observer.disconnect();
+            }
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
+});
