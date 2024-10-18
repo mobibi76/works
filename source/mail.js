35b6a4244027b1bf14dba7b34b67a68a7212b1f2
mobibi76 <snowfall.hamn@gmail.com>
2024-10-18 21:19:01 +0900

    try

--- source/mail.js
+++ source/mail.js
@@ -1,2 +1,9 @@
-document.getElementById('email').innerHTML =
-    '<a href="mailto:processdesignbase@gmail.com">processdesignbase@gmail.com</a> (125-51-00257)';
+document.addEventListener('DOMContentLoaded', () => {
+    const emailElement = document.getElementById('email');
+    if (emailElement) {
+        emailElement.innerHTML = 
+            '<a href="mailto:processdesignbase@gmail.com">processdesignbase@gmail.com</a> (125-51-00257)';
+    } else {
+        console.error('Email Element Not Found.');
+    }
+});
