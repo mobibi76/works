62de618bd04cb8861f13c9ad31eb21c58ec8f074
mobibi76 <snowfall.hamn@gmail.com>
2024-10-18 21:40:31 +0900

    try

--- source/main.js
+++ source/main.js
@@ -33,6 +33,12 @@
                 containerElement.innerHTML = text;
                 containerElement.scrollTop = 0;
                 bindInterLinkEvent();
+
+                const emailScript = document.createElement('script');
+                emailScript.src = './source/email.js';
+                emailScript.type = 'module';
+                document.body.appendChild(emailScript);
+
                 if (pageTitle.includes('Demo')) {
                     loadIframeWithTimeout('iframe', 'https://test.pdbops.com:8000/test/', 3500);
                 }
